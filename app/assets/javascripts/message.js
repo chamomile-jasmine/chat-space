7200e3e6432a717ebadf691661f8260c2081b863
chamomile-jasmine <abee_acubic2017@yahoo.co.jp>
2019-07-09 21:05:18 +0900

    2nd差分のみ表示を追加するよう設定
    ・メッセージ送信の非同期通信化（メッセージを送信するたびに送信画面にリダイレクトしているため、ビューが毎回再描画されてしまうため）
    ・コメントアウトとデバッグ用の記載を消去した

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -31,16 +31,13 @@ $(function() {
     $.ajax({
       type: 'POST',
       url: location.href,
-      // group/1/messages
       data: formdata,
       dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data) {
-      console.log('aaaaaa')
       var message = buildHTML(data);
-      // console.log(message)
       $('.messages').append(message);
       $('.form__message').val('');
       $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight}, 'fast');
@@ -52,39 +49,5 @@ $(function() {
   return false;
   });
 
-  // メッセージ自動更新
-//     var interval = setInterval(function() {
-//       if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-//     $.ajax({
-//       type: 'GET',
-//       url: location.href,
-//       dataType: 'json'
-//     })
-//     .done(function(data) {
-//       var id = $('.chat').data('messageId');
-//       var insertHTML = '';
-//       data.messages.forEach(function(message) {
-//         if (message.id > id ) {
-//           insertHTML += buildHTML(message);
-//         }
-//       });
-//       $('.chat-wrapper').prepend(insertHTML);
-//     })
-//     .fail(function(data) {
-//       alert('自動更新に失敗しました');
-//     });
-//   } else {
-//     clearInterval(interval);
-//    }} , 5 * 1000 );
+
 });
-
-
-// 備忘
-// var html = `
-// <div class="message" data-message-id="${message.id}">
-//   <p class="message__user">${message.name}</p>
-//   <p class="message__date">${message.date}</p>
-//   <p class="lower-message__content">${message.body}</p>
-//   ${insertImage}
-// </div>`;
-// return html
